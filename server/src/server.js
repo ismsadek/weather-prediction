dee7c877e7ae92e30d8a09ceb406b524142f1af7
migohe14 <migohe14@gmail.com>
2018-09-24 11:59:58 +0200

    server.js

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+//Settings
+//*process.env.PORT  Esta cogiendo el puerto que te da el sistema operativo
+app.set('port', process.env.PORT || 3000);
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/weather', require('./routes/api'));
+
+
+//Server is listenning
+app.listen(app.get('port'), () => {
+    console.log("Server en puerto", app.get('port'))
+})
