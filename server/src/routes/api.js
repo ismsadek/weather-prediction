122aff8dcab59f3e9b1fccd474228344846a9cc0
migohe14 <migohe14@gmail.com>
2018-09-24 12:29:04 +0200

    api-openweathermap

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -7,16 +7,18 @@ const request = require('request');
 router.get('/', async (req, res) => {
         //Aquí va la url de openweathermap con la key 
 
-    request('', function (error, response, body) {
+    request('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1af7acad75875839f06098421676974b', function (error, response, body) {
         console.log('error:', error); 
         console.log('statusCode:', response && response.statusCode); 
         console.log('body:', body); 
-        });
-    
+        // res.json(body);
         res.send({
-            success: true,
-            message: "llamada get correcta"
+            body: body
         });
+
+        });
+        
+       
 });
 
 
